8531d2d185aa42f98dc9fde0869f766e49a2bf87
younghk12 <hkyoung503@gmail.com>
2026-04-04 21:46:45 -0700

    nav link error

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,9 +17,9 @@ export function initNavbar() {
         <div class="drawer">
             <ul>
                 <li class="home"><a href="/">Home</a></li>
-                <li><a href="/details">Details</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/details/">Details</a></li>
+                <li><a href="/about/">About</a></li>
+                <li><a href="/contact/">Contact</a></li>
             </ul>
         </div>
     </nav>
